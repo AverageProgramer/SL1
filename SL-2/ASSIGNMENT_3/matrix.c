c03d0f5274cb4556ac2674b96afd960e29d1d48b
agrawaltejas01 <agrawaltejas01@gmail.com>
2018-07-27 15:09:03 +0530

    Next

---
+++ SL-2/ASSIGNMENT_3/matrix.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<math.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<malloc.h>
+#include<stdlib.h>
+
+int r,c;
+int a[20][20],b[20][20],ans[20][20];
+
+typedef struct position
+{
+	int a,b;
+}position;
+
+void * thread_matrix_mult(void *arg)
+{
+	position *m = (position*)arg;
+
+	int sum=0;
+	for(int i=0;i<r;i++)
+	{
+		sum += a[m->a][i] * b[i][m->b];
+	}
+
+	ans[m->a][m->b] = sum;
+
+}
+
+void Read()
+{
+	printf("Enter number of row : ");
+	scanf("%d",&r);
+	printf("\n");
+
+	printf("Enter number of column : ");
+	scanf("%d",&c);
+	printf("\n");
+
+	printf("Enter matrix 1 :");
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+			scanf("%d",&a[i][j]);
+	}
+
+	printf("Enter matrix 2 :");
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+			scanf("%d",&b[i][j]);
+	}
+
+	printf("\n\n");
+}
+
+int  main()
+{
+	Read();
+
+	int count=0;
+	pthread_t arr[400];	//create array of threads
+
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			position *pos = (position*)malloc(sizeof(position));
+			pos -> a=i;
+			pos -> b=j;
+
+			pthread_create(&arr[count++],NULL,thread_matrix_mult,(void *)pos);
+		}
+	}
+
+	for(int i=0;i<count;i++)
+		pthread_join(arr[i],NULL);
+
+	for(int i=0;i<r;i++)			//printing ans
+	{
+		for(int j=0;j<c;j++)
+		{
+			printf("%d ",ans[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+
